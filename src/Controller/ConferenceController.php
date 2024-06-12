490257a358e4c928f3c88c23cd26da96fe671668
ura2rist <ura2rist@gmail.com>
2024-06-12 17:08:05 +0300

    база

--- src/Controller/ConferenceController.php
+++ src/Controller/ConferenceController.php
@@ -9,18 +9,12 @@ use Symfony\Component\Routing\Attribute\Route;
 
 class ConferenceController extends AbstractController
 {
-    #[Route('/hello/{$name}', name: 'homepage')]
-    public function index(string $name): Response
+    #[Route('/', name: 'homepage')]
+    public function index(): Response
     {
-      $greet = '';
-
-      if($name = $request->query->get('hello')) {
-        $greet = sprintf('<h1>Hello %s!</h1>', htmlspecialchars($name));
-      }
       return new Response(<<<EOF
         <html>
           <body>
-            $greet
             <img src="/images/under-construction.gif" />
           </body>
         </html>
